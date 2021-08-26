d941f2c6edecfb9385ceff260fd6e7b9c39a7c0b
satori <huan0016@gmail.com>
2021-08-26 20:47:47 +0800

    debug

--- start.sh
+++ start.sh
@@ -3,8 +3,4 @@ git pull
 chmod 777 start.sh
 
 pm2 delete shizukana
-<<<<<<< HEAD
 pm2 start node --name shizukana -- index
-=======
-pm2 start node --name shizukana -- index
->>>>>>> 7de76a2a75600de393bda88d8893d31f40a4685c
